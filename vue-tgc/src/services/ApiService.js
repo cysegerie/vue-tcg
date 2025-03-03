5f82020c1323fcb958c6c0b0e95563f4f91b5b50
cyriaque.segerie <Cyriaque.SEGERIE@etu.uca.fr>
2025-03-03 18:25:22 +0100

    ajout ApiService

--- vue-tgc/src/services/ApiService.js
+++ vue-tgc/src/services/ApiService.js
@@ -1,45 +1,36 @@
-class ApiService {
-    constructor(baseURL) {
-        if (!baseURL) {
-            throw new Error('Base URL is required');
-        }
-        this.baseURL = baseURL;
-    }
-
-    async get(resource) {
-        const url = new URL(`${this.baseURL}/${resource}`);
-        const response = await fetch(url);
-        return response.json();
-    }
-
-    async post(resource, data) {
-        const response = await fetch(`${this.baseURL}/${resource}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return response.json();
-    }
-
-    async update(resource, data) {
-        const response = await fetch(`${this.baseURL}/${resource}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return response.json();
-    }
-
-    async delete(resource) {
-        const response = await fetch(`${this.baseURL}/${resource}`, {
-            method: 'DELETE'
-        });
-        return response.json();
-    }
+async function apiGet(resource) {
+    const url = new URL(`${this.baseURL}/${resource}`);
+    const response = await fetch(url);
+    return response.json();
 }
 
-export default ApiService;
+async function apiPost(resource, data) {
+    const response = await fetch(`${this.baseURL}/${resource}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+async function apiUpdate(resource, data) {
+    const response = await fetch(`${this.baseURL}/${resource}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+async function apiDelete(resource) {
+    const response = await fetch(`${this.baseURL}/${resource}`, {
+        method: 'DELETE'
+    });
+    return response.json();
+}
+
+export default { apiGet, apiPost, apiUpdate, apiDelete };
