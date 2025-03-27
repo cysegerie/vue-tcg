ecba08d34490541a78cdcdab2b8544cefe138ba9
Segerie Cyriaque <segeriecyriaque@gmail.com>
2025-03-27 17:45:54 +0100

    ajout placeholder q4

--- vue-tcg/src/components/AllMyCards.vue
+++ vue-tcg/src/components/AllMyCards.vue
@@ -50,7 +50,7 @@ watch([currentPage, itemsPerPage], ([newPage, newItemsPerPage], [oldPage, oldIte
             <div class="cards-container">
                 <div v-for="card in cards" :key="card.id" class="card-item">
                     <RouterLink :to="`/cards/${card.id}`" class="card-link">
-                        <img :src="`${card.image}/low.jpg`" alt="Card Image" />
+                        <img :src="card.image ? `${card.image}/low.jpg` : '/placeholder.jpg'" alt="Card Image" />
                         <p>{{ card.name }}</p>
                     </RouterLink>
                 </div>
